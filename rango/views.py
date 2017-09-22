67343409890c482edd92022af921dc8dd7b9c592
Tony Okong'o <tonyvevo@gmail.com>
2017-09-23 02:08:43 +0300

    4. Templates, Static & Media files

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br> <a href='/rango/about/'>About</a>")
+    # A dictionary to pass to the template engine as its content
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br> <a href='/rango/'>Main Page</a>")
+    # A dictionary to pass my name to the template engine as its content
+    name_dict = {'my_name': "TonyVEVO"}
+    return render (request, 'rango/about.html', context=name_dict)
