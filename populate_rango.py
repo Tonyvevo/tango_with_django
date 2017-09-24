26ff9094280711923a523857d34d203fc1d466a5
Tony Okong'o <tonyvevo@gmail.com>
2017-09-24 14:59:44 +0300

    6. SlugField, Parametized URL Mapping & Category Page

--- populate_rango.py
+++ populate_rango.py
@@ -13,25 +13,32 @@ def populate():
 
     python_pages = [
         {"title":"Official Python Tutorial",
-         "url":"http://docs.python.org/2/tutorial/"},
+         "url":"http://docs.python.org/2/tutorial/",
+         "views": 1024},
         {"title":"How to think like a Computer Scientist",
-         "url":"http://www.greenteapress.com/thinkpython/"},
+         "url":"http://www.greenteapress.com/thinkpython/",
+         "views": 512},
         {"title":"Learn Python in 10 Minutes",
-         "url":"http://www.korokithakis.net/tutorials/python/"}
+         "url":"http://www.korokithakis.net/tutorials/python/",
+         "views": 64}
         ]
 
     django_pages = [
         {"title":"Official Django Tutorial",
-         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 768},
         {"title":"How to Tango with Django",
-         "url":"http://www.tangowithdjango.com/"}
+         "url":"http://www.tangowithdjango.com/",
+         "views": 256}
         ]
 
     other_pages = [
         {"title":"Bottle",
-         "url":"http://bottlepy.org/docs/dev/"},
+         "url":"http://bottlepy.org/docs/dev/",
+         "views": 8},
         {"title":"Flask",
-         "url":"http://flask.pocoo.org"}
+         "url":"http://flask.pocoo.org",
+         "views": 32}
         ]
 
     # Dictionary of dictionaries for our categories
@@ -52,7 +59,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     # Print out the categories we have added
 
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    # A dictionary to pass to the template engine as its content
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+from rango.models import Category, Page
+
+def index(request):    
+    # Query db for first 5 categories with highest likes
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    # Query db for first 5 pages with most views
+    page_list = Page.objects.order_by('-views')[:5]
+
+    # Add these lists to the template context
+    context_dict = {'categories': category_list,
+                    'pages': page_list}
+    
     return render(request, 'rango/index.html', context=context_dict)
 
-def about(request):
-    # A dictionary to pass my name to the template engine as its content
-    name_dict = {'my_name': "TonyVEVO"}
-    return render (request, 'rango/about.html', context=name_dict)
+def about(request):    
+    # A dictionary to pass my name to the template renderer
+    context_dict = {'my_name': "TonyVEVO"}
+    return render (request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    # The context dictionary to pass to the template renderer
+    context_dict = {}
+
+    try:
+        # Try to find a category with the provided name-slug 
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retreive all pages associated to the given category
+        pages = Page.objects.filter(category=category)
+
+        # Add the list of pages to the template context
+        context_dict['pages'] = pages
+
+        # Add the existing category object to the template context
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        # Add None to the template context
+        # for the non-existing category
+        context_dict['category']=None
+        context_dict['pages']=None
+        
+    return render(request, 'rango/category.html', context_dict)
+
+        
